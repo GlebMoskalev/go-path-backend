937053af993e3550e53507542a44590ac6b2703e
GlebMoskalev <gleb12122003@gmail.com>
2026-05-10 10:21:37 +0500

    fix

--- content/tasks/02-types/02-parse-price/template.go
+++ content/tasks/02-types/02-parse-price/template.go
@@ -1,10 +1,7 @@
 package solution
 
-import "strconv"
-
 // ParsePrice разбирает строку с ценой и возвращает значение и признак успеха.
 func ParsePrice(s string) (float64, bool) {
 	// Напишите ваш код здесь
-	_ = strconv.ParseFloat
 	return 0, false
 }
